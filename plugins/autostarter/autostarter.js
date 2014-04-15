e4d5cf902e2cc2393575e2d252a0d7a55057ec7f
kmeaw <kmeaw@larkit.ru>
2014-04-15 18:15:48 +0400

    Autostarter with cookies

--- plugins/autostarter/autostarter.js
+++ plugins/autostarter/autostarter.js
@@ -1,20 +1,40 @@
 var spawn = require('child_process').spawn;
 var Q = require('q');
+var crypto = require('crypto');
 var daemon = null;
+var fs = require('fs');
+var path = require('path');
+
+var homepath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+var cookie_path = path.join(homepath, ".empty.cookie");
 
 module.exports.init = function init(emptyc) {
   if (emptyc.config("autostarter.disable"))
     return Q.resolve();
-  return emptyc.napi("ping").then(function() { return Q.resolve(); }, function() {
+  return emptyc.napi("ping").then(function() { return Q.resolve(); }, function(err) {
+    if (err.cookie == "bad" && fs.existsSync(cookie_path))
+      emptyc.config("cookie", fs.readFileSync(cookie_path, {encoding:"utf8"}));
+  }).then(function() { return emptyc.napi("ping") })
+    .then(function() { return Q.resolve(); }, function(err) {
+    if (err.cookie == "bad")
+      throw new Error('Bad cookie');
     if (!emptyc.config("autostarter.quiet"))
       console.log("autostarter: Daemon ping failed, spawning a new one...");
-    daemon = spawn('emptyd', [], { stdio: 'pipe' });
+    emptyc.config("cookie", crypto.randomBytes(16).toString('hex'));
+    var buffer = "";
+    if (emptyc.config("autostarter.remote"))
+      daemon = spawn('ssh', [emptyc.config("autostarter.remote"),"-L",emptyc.config("server_port")+":localhost:"+emptyc.config("server_port"),"emptyd","--cookie", emptyc.config("cookie")], { stdio: 'pipe' });
+    else
+      daemon = spawn('emptyd', ["--cookie", emptyc.config("cookie"), "--port", emptyc.config("server_port")], { stdio: 'pipe' });
+    var oldmask = process.umask(077);
+    fs.writeFileSync(cookie_path, emptyc.config("cookie"));
+    process.umask(oldmask);
     daemon.on("error", function(err) {
       console.error("autostarter: Daemon start failed: " + err + ", please try running emptyd manually.");
+      console.error(buffer);
     });
     daemon.stdout.setEncoding('utf-8');
     daemon.stderr.setEncoding('utf-8');
-    var buffer = "";
     fill = function(chunk)
     {
       buffer = (buffer + chunk).substr(-4096);
@@ -23,7 +43,10 @@ module.exports.init = function init(emptyc) {
     daemon.stderr.on('data', fill);
     daemon.on('close', function(code) {
       if (code !== 0)
+      {
         console.error("Daemon exited with code %d.", code);
+	console.error(buffer);
+      }
       daemon = null;
     });
   });
@@ -33,6 +56,9 @@ module.exports.fini = function fini(emptyc) {
   if (daemon)
   {
     daemon.kill();
+    try {
+      fs.unlinkSync(cookie_path);
+    } catch(e) {}
     if (!emptyc.config("autostarter.quiet"))
       console.log("Shutting down the daemon...");
   }
